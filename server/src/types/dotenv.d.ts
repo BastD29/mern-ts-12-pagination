9edfaf7244b9514ae8bfef087be2e5ab08188227
BastD29 <bastiendaniel9@gmail.com>
2024-08-14 16:19:09 +0200

    add dotenv.d.ts

---
+++ server/src/types/dotenv.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production" | "test" | "local" | "staging";
+    PORT: string;
+    MONGO_URI: string;
+    ALLOWED_ORIGINS: string;
+  }
+}
