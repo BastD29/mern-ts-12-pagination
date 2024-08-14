0c54071722578c8190f5a984051212a32d17c087
BastD29 <bastiendaniel9@gmail.com>
2024-08-14 16:17:16 +0200

    add cors config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,17 @@
 import express from "express";
 import cors from "cors";
+import { NODE_ENV, PORT } from "./config/environments";
+import { connectDB } from "./config/db";
+import { corsOptions } from "./config/cors";
+
+connectDB();
+
+const app = express();
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}, ${NODE_ENV} environment`);
+});
