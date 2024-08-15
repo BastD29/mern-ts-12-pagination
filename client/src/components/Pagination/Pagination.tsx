17fc2e1f01e00a0b3e45b62c781f4b73d8c4607f
BastD29 <bastiendaniel9@gmail.com>
2024-08-15 11:09:01 +0200

    complete pagination component part I

--- client/src/components/Pagination/Pagination.tsx
+++ client/src/components/Pagination/Pagination.tsx
@@ -1,10 +1,45 @@
 import { FC } from "react";
+import { usePaginationContext } from "../../hooks/usePaginationContext";
 import style from "./Pagination.module.scss";
 
 const Pagination: FC = () => {
+  const {
+    dispatch,
+    state: { pagination },
+  } = usePaginationContext();
+  const { page, totalItems, totalPages } = pagination;
+
+  const renderPageNumbers = () => {
+    const pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(
+        <button
+          key={i}
+          // onClick={() => handlePageChange(i)}
+          disabled={i === page}
+        >
+          {i}
+        </button>
+      );
+    }
+    return pages;
+  };
+
   return (
     <div className={style["pagination"]}>
-      <h2>Pagination</h2>
+      <button
+        disabled={page <= 1}
+        // onClick={() => handlePageChange(paginationState.page - 1)}
+      >
+        Previous
+      </button>
+      {renderPageNumbers()}
+      <button
+        disabled={page >= totalPages}
+        // onClick={() => handlePageChange(paginationState.page + 1)}
+      >
+        Next
+      </button>
     </div>
   );
 };
