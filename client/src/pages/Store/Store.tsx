57b4f95aa1abedd9c7e5ca3ddc79ce9472a8a03b
BastD29 <bastiendaniel9@gmail.com>
2024-08-15 10:56:07 +0200

    add pagination component

--- client/src/pages/Store/Store.tsx
+++ client/src/pages/Store/Store.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import ItemsList from "../../components/ItemsList/ItemsList";
+import Pagination from "../../components/Pagination/Pagination";
 import style from "./Store.module.scss";
 
 const Store: FC = () => {
   return (
     <div className={style["store"]}>
       <ItemsList />
+      <Pagination />
     </div>
   );
 };
