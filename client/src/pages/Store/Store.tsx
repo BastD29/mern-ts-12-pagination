950b167c6c25e9aa4ee272ecb3380efc67be9909
BastD29 <bastiendaniel9@gmail.com>
2024-08-15 15:01:34 +0200

    add filter in store

--- client/src/pages/Store/Store.tsx
+++ client/src/pages/Store/Store.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import ItemsList from "../../components/ItemsList/ItemsList";
 import Pagination from "../../components/Pagination/Pagination";
+import Filter from "../../components/Filter/Filter";
 import style from "./Store.module.scss";
 
 const Store: FC = () => {
   return (
     <div className={style["store"]}>
+      <Filter />
       <ItemsList />
       <Pagination />
     </div>
